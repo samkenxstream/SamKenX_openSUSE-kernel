4bee6d888d56023420a2078b2154ddf97487cca2
Michal Kubecek <mkubecek@suse.cz>
2021-10-28 19:29:16 +0200

    net: sched: add empty status flag for NOLOCK qdisc (bsc#1183405).
    suse-commit: 750f1869474f867f105a811f03b3a92f21e25a5f

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -673,6 +673,8 @@ static struct sk_buff *pfifo_fast_dequeue(struct Qdisc *qdisc)
 		qdisc_qstats_cpu_backlog_dec(qdisc, skb);
 		qdisc_bstats_cpu_update(qdisc, skb);
 		qdisc_qstats_atomic_qlen_dec(qdisc);
+	} else {
+		qdisc->empty = true;
 	}
 
 	return skb;
@@ -895,6 +897,7 @@ struct Qdisc *qdisc_alloc(struct netdev_queue *dev_queue,
 	sch->enqueue = ops->enqueue;
 	sch->dequeue = ops->dequeue;
 	sch->dev_queue = dev_queue;
+	sch->empty = true;
 	dev_hold(dev);
 	refcount_set(&sch->refcnt, 1);
 
