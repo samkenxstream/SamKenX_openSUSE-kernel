8553191df24b72076d5d158aa988360369f83689
Miroslav Benes <mbenes@suse.cz>
2021-12-17 12:38:30 +0100

    ring-buffer: Protect ring_buffer_reset() from reentrancy (CVE-2020-27825 bsc#1179960).
    suse-commit: 432ad3d863d43e1315b57e441d408b79ede8934d

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -4336,6 +4336,9 @@ void ring_buffer_reset(struct ring_buffer *buffer)
 	struct ring_buffer_per_cpu *cpu_buffer;
 	int cpu;
 
+	/* prevent another thread from changing buffer sizes */
+	mutex_lock(&buffer->mutex);
+
 	for_each_buffer_cpu(buffer, cpu) {
 		cpu_buffer = buffer->buffers[cpu];
 
@@ -4354,6 +4357,8 @@ void ring_buffer_reset(struct ring_buffer *buffer)
 		atomic_dec(&cpu_buffer->record_disabled);
 		atomic_dec(&cpu_buffer->resize_disabled);
 	}
+
+	mutex_unlock(&buffer->mutex);
 }
 EXPORT_SYMBOL_GPL(ring_buffer_reset);
 
