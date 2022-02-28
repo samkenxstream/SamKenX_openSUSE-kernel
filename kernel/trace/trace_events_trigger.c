555270bd85baf4b9ea76a00763b18a5904b1325b
Miroslav Benes <mbenes@suse.cz>
2022-02-28 11:34:49 +0100

    tracing: Dump stacktrace trigger to the corresponding instance (git-fixes).
    suse-commit: f5d1861ee2a0c0c8d7e9bfbc64d70102919a23ef

--- kernel/trace/trace_events_trigger.c
+++ kernel/trace/trace_events_trigger.c
@@ -1162,7 +1162,17 @@ static __init int register_trigger_snapshot_cmd(void) { return 0; }
 static void
 stacktrace_trigger(struct event_trigger_data *data, void *rec)
 {
-	trace_dump_stack(STACK_SKIP);
+	struct trace_event_file *file = data->private_data;
+	unsigned long flags;
+	int pc;
+
+	if (file) {
+		local_save_flags(flags);
+		pc = preempt_count();
+		__trace_stack(file->tr, flags, STACK_SKIP, pc);
+	} else {
+		trace_dump_stack(STACK_SKIP);
+	}
 }
 
 static void
