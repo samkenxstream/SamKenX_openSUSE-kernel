0015bbab0661b64e0ed113cd3092025c2f938081
Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>
2022-04-03 11:05:41 +0200

    RDMA/rxe: Handle skb_clone() failure in rxe_recv.c (git-fixes)
    - refresh: patches.suse/RDMA-rxe-Fix-coding-error-in-rxe_rcv_mcast_pkt.patch
    
    suse-commit: c27a191288bf8592495f8eba0270a8b02a05fe65

--- drivers/infiniband/sw/rxe/rxe_recv.c
+++ drivers/infiniband/sw/rxe/rxe_recv.c
@@ -330,6 +330,9 @@ static void rxe_rcv_mcast_pkt(struct rxe_dev *rxe, struct sk_buff *skb)
 			skb = NULL;
 		}
 
+		if (unlikely(!per_qp_skb))
+			continue;
+
 		per_qp_pkt = SKB_TO_PKT(per_qp_skb);
 		per_qp_pkt->qp = qp;
 		rxe_add_ref(qp);
