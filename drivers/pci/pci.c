5965ea4480e6f17c45231e2751a58b7363aed1b3
Jiri Slaby <jslaby@suse.cz>
2022-04-26 08:54:27 +0200

    PCI: Fix overflow in command-line resource alignment requests (git-fixes).
    suse-commit: 93779e7bac6016015366229979eeb7ed073cadcd

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -5668,7 +5668,7 @@ static resource_size_t pci_specified_resource_alignment(struct pci_dev *dev,
 				if (align_order == -1)
 					align = PAGE_SIZE;
 				else
-					align = 1 << align_order;
+					align = 1ULL << align_order;
 				/* Found */
 				break;
 			}
@@ -5694,7 +5694,7 @@ static resource_size_t pci_specified_resource_alignment(struct pci_dev *dev,
 				if (align_order == -1)
 					align = PAGE_SIZE;
 				else
-					align = 1 << align_order;
+					align = 1ULL << align_order;
 				/* Found */
 				break;
 			}
